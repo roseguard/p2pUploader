6618c40a21c3d17dc0cb7b456974d42e57901361
Roseguard <roseguardd@gmail.com>
2017-12-24 04:58:24 +0300

    Added connecting to other ports and more info for user

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ int main(int argc, char *argv[])
     if(QCoreApplication::arguments().length()<2)
     {
         qDebug() << "Need argument (path to the uploading file)";
+        qDebug() << "";
+        qDebug() << "If you run it from desktop drag and drop file for uploading on the icon of this programm in system (not here, this one you may close)";
+        qDebug() << "";
+        qDebug() << "If you running programm from console - put path to the uploading file as argument";
         return a.exec();
     }
     qDebug() << "Selected : " << QCoreApplication::arguments().at(1);
